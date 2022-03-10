c9bdec2c97862c254699ac868ebd576c1e67325d
EngelibertTom <82039753+EngelibertTom@users.noreply.github.com>
2022-03-10 13:08:21 +0100

    start

--- code/client/src/login/Login.js
+++ code/client/src/login/Login.js
@@ -2,61 +2,38 @@ import React from 'react';
 import axios from "axios";
 import {Link} from 'react-router-dom';
 import {useCookies, withCookies} from 'react-cookie';
-import './Login.css';
 
 function FormLogin(props) {
     return (
-        <div className='flex flex-center'>
         <form onSubmit={props.onSignin}>
             <div>
-                <label>Email</label>
-                <input type="text" id="username" autoComplete="off" ref={props.usernameRef}/>
+                <label>email:</label>
+                <input type="text" id="email" autoComplete="off" ref={props.emailRef}/>
             </div>
             <div>
-                <label>Password</label>
+                <label>Password:</label>
                 <input type="password" name="password" autoComplete="off" ref={props.passwordRef}/>
             </div>
             <div>
                 <button type="submit" name="login">Login</button>
-                <button type="button" name="signup" onClick={props.onSignup}>
-                    Sign up
-                </button>
             </div>
         </form>
-        </div>
-
     );
 }
 
 function Login() {
     const [cookies, setCookie, removeCookie] = useCookies(['login']);
-    const usernameRef = React.createRef();
+    const emailRef = React.createRef();
     const passwordRef = React.createRef();
 
     function disconnect() {
         removeCookie('login');
     }
 
-    async function onSignup() {
-        const user = {
-            username: usernameRef.current.value,
-            password: passwordRef.current.value
-        };
-        try {
-            const p = (await axios.post('http://localhost:8000/signup', user));
-            if (p.status === 200) {
-                user.token = p.data.token;
-                setCookie('login', user, '/');
-            }
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
     async function onSignin(e) {
         e.preventDefault();
         const user = {
-            username: e.target.username.value,
+            email: e.target.email.value,
             password: e.target.password.value
         };
         try {
@@ -73,13 +50,13 @@ function Login() {
     if (cookies.login && cookies.login.token) {
         return <button id="disconnect" onClick={disconnect}>disconnect</button>;
     }
-    return <FormLogin onSignin={onSignin} onSignup={onSignup} usernameRef={usernameRef} passwordRef={passwordRef}/>
+    return <FormLogin onSignin={onSignin} emailRef={emailRef} passwordRef={passwordRef}/>
 }
 
 function LocalProtectedRoute({children, ...rest}) {
-    if (rest.allCookies && rest.allCookies.login && rest.allCookies.login.username && rest.allCookies.login.token) {
+    if (rest.allCookies && rest.allCookies.login && rest.allCookies.login.email && rest.allCookies.login.token) {
         return (
-            React.cloneElement(children, {username: rest.allCookies.login.username, token: rest.allCookies.login.token})
+            React.cloneElement(children, {email: rest.allCookies.login.email, token: rest.allCookies.login.token})
         )
     }
     return <></>
@@ -90,15 +67,26 @@ function LocalProtectedRoute({children, ...rest}) {
  * @return {null}
  */
 function LocalProtectedLink({...rest}) {
-    if (rest.allCookies && rest.allCookies.login && rest.allCookies.login.username && rest.allCookies.login.token) {
-        return <Link className={rest.className} to={rest.to}>cities</Link>
+    if (rest.allCookies && rest.allCookies.login && rest.allCookies.login.email && rest.allCookies.login.token) {
+        return <Link className={rest.className} to={rest.to}>{rest.children}</Link>
     } else {
         return null;
     }
 }
 
+
+function NotLocalProtectedLink({...rest}) {
+    if (!(rest.allCookies && rest.allCookies.login && rest.allCookies.login.email && rest.allCookies.login.token)) {
+        return <Link className={rest.className} to={rest.to}>{rest.children}</Link>
+    } else {
+        return null;
+    }
+}
+
+
 const ProtectedRoute = withCookies(LocalProtectedRoute);
 const ProtectedLink = withCookies(LocalProtectedLink);
+const NotProtectedLink = withCookies(NotLocalProtectedLink);
 
-export {ProtectedRoute, ProtectedLink};
+export {ProtectedRoute, ProtectedLink, NotProtectedLink};
 export default Login;
