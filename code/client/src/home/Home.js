f80df9a97fe7a60422d6ca6281a9bc6d38f8fcb1
YueXingTong <d00234628@student.dkit.ie>
2022-03-11 15:53:50 +0000

    merge2

--- code/client/src/home/Home.js
+++ code/client/src/home/Home.js
@@ -5,74 +5,74 @@ import fleche from '../images/fleche.png'
 export default function Home() {
     return (
 
-    <section class="home container">
-        <div class="article_une">
-            <h1 class="titre">Main article name</h1>
-            <img class="img" src={image} alt="" />
-            <span class="desc"> Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam congue pretium eros. Fusce volutpat libero ligula, ut luctus felis ornare a. Nullam varius, sem quis eleifend feugiat, lectus justo hendrerit dolor, nec placerat velit leo id massa.</span>
+    <section className="home container">
+        <div className="article_une">
+            <h1 className="titre">Main article name</h1>
+            <img className="img" src={image} alt="" />
+            <span className="desc"> Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam congue pretium eros. Fusce volutpat libero ligula, ut luctus felis ornare a. Nullam varius, sem quis eleifend feugiat, lectus justo hendrerit dolor, nec placerat velit leo id massa.</span>
         </div>
 
-        <div class="dernier_articles">
-            <div class="div_titre_last"> <img class="titre_img" src={fleche} alt="" /><h2 class="titre">Last articles : </h2> </div>
-            <div class="article">
+        <div className="dernier_articles">
+            <div className="div_titre_last"> <img className="titre_img" src={fleche} alt="" /><h2 className="titre">Last articles : </h2> </div>
+            <div className="article">
                 <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <span className="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
             </div>
 
-            <div class="article">
+            <div className="article">
                 <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <span className="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
             </div>
 
-            <div class="article">
+            <div className="article">
                 <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <span className="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
             </div>
 
-            <div class="article">
+            <div className="article">
                 <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <span className="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
             </div>
         </div>
 
-        <div class="categories">
-            <div class="div_titre_last"> <img class="titre_img" src={fleche} alt="" /><h2 class="titre">Categories : </h2> </div>
+        <div className="categories">
+            <div className="div_titre_last"> <img className="titre_img" src={fleche} alt="" /><h2 className="titre">Categories : </h2> </div>
 
-            <div class="div_cat">
+            <div className="div_cat">
                 
-                <a href="" class="cat1">
+                <a href="" className="cat1">
                     <span>News</span>
                 </a>
 
-                <a href="" class="cat2">
+                <a href="" className="cat2">
                     <span>How to swim ?</span>
                 </a>
 
-                <a href="" class="cat3">
+                <a href="" className="cat3">
                     <span>discover our tips for better swimming</span>
                 </a>
 
-                <a href="" class="cat4">
+                <a href="" className="cat4">
                     <span>Comptetitions</span>
                 </a>
 
-                <a href="" class="cat5">
+                <a href="" className="cat5">
                     <span>World records</span>
                 </a>
 
-                <a href="" class="cat6">
+                <a href="" className="cat6">
                     <span>Athlets</span>
                 </a>
                 
-                <a href="" class="cat7">
+                <a href="" className="cat7">
                     <span>Awards</span>
                 </a>
 
-                <a href="" class="cat">
+                <a href="" className="cat">
                     <span>Best men swimming</span>
                 </a>
 
-                <a href="" class="cat">
+                <a href="" className="cat">
                     <span>Best women swimming</span>
                 </a>
             </div>
