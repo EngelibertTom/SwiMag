f2e08aa711c367bfa963b005f8a8a88526105e40
Rémi <craco38play@gmail.com>
2022-03-11 18:19:18 +0100

    home articles

--- code/client/src/home/Home.js
+++ code/client/src/home/Home.js
@@ -1,5 +1,8 @@
 import './Home.css';
 import image from '../images/image_test.png'
+import image2 from '../images/image_test2.png'
+import image3 from '../images/image_test3.png'
+import image4 from '../images/image_test4.png'
 import fleche from '../images/fleche.png'
 
 export default function Home() {
@@ -7,32 +10,41 @@ export default function Home() {
 
     <section class="home container">
         <a href="" class="article_une">
-            <h1 class="titre">Main article name</h1>
+            <h1 class="titre">Practice Is Everything: The Mindset Behind Superhero Workouts</h1>
             <img class="img" src={image} alt="" />
-            <span class="desc"> Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam congue pretium eros. Fusce volutpat libero ligula, ut luctus felis ornare a. Nullam varius, sem quis eleifend feugiat, lectus justo hendrerit dolor, nec placerat velit leo id massa.</span>
+            <span class="desc"> If you are serious about accomplishing big things in the pool you need to rethink the way you train. Here is why practice is everything.</span>
         </a>
 
         <div class="dernier_articles">
             <div class="div_titre_last"> <img class="titre_img" src={fleche} alt="" /><h2 class="titre">Last articles : </h2> </div>
             <a href="" class="article">
                 <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <div class="desc">
+                    <span class="titre">The Top 6 Core Exercises for Swimmers</span>
+                    <span>Having a powerful core will make you a better, stronger swimmer. </span>
+                </div>
             </a>
 
             <a href="" class="article">
-                <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-            </a>
+                <img src={image2} alt="" />
+                <div class="desc">
+                    <span class="titre">How to Swim Faster Freestyle</span>
+                    <span>Ready to take your freestyle to the next level?</span>
+                </div>            </a>
 
             <a href="" class="article">
-                <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-            </a>
+                <img src={image3} alt="" />
+                <div class="desc">
+                    <span class="titre">Butterfly Sets: How to Dominate Your Next 100m Butterfly</span>
+                    <span>“You gotta go out fast, and come back faster!” is one of those lines ...</span>
+                </div>            </a>
 
             <a href="" class="article">
-                <img src={image} alt="" />
-                <span class="desc">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-            </a>
+                <img src={image4} alt="" />
+                <div class="desc">
+                    <span class="titre">This is How Fast Katie Ledecky Swims in Practice</span>
+                    <span>You already know that Katie Ledecky is the top female swimmer ...</span>
+                </div>            </a>
         </div>
 
         <div class="categories">
@@ -78,6 +90,7 @@ export default function Home() {
             </div>
 
         </div>
+
     </section>
 
     );
