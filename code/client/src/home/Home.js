d0bb77973e70c0c08c9680aba15999ca84d321cc
Rémi <craco38play@gmail.com>
2022-03-11 18:34:22 +0100

    home images

--- code/client/src/home/Home.js
+++ code/client/src/home/Home.js
@@ -3,6 +3,7 @@ import image from '../images/image_test.png'
 import image2 from '../images/image_test2.png'
 import image3 from '../images/image_test3.png'
 import image4 from '../images/image_test4.png'
+import image5 from '../images/image_test5.png'
 import fleche from '../images/fleche.png'
 
 export default function Home() {
@@ -18,7 +19,7 @@ export default function Home() {
         <div class="dernier_articles">
             <div class="div_titre_last"> <img class="titre_img" src={fleche} alt="" /><h2 class="titre">Last articles : </h2> </div>
             <a href="" class="article">
-                <img src={image} alt="" />
+                <img src={image2} alt="" />
                 <div class="desc">
                     <span class="titre">The Top 6 Core Exercises for Swimmers</span>
                     <span>Having a powerful core will make you a better, stronger swimmer. </span>
@@ -26,23 +27,23 @@ export default function Home() {
             </a>
 
             <a href="" class="article">
-                <img src={image2} alt="" />
+                <img src={image3} alt="" />
                 <div class="desc">
                     <span class="titre">How to Swim Faster Freestyle</span>
                     <span>Ready to take your freestyle to the next level?</span>
                 </div>            </a>
 
             <a href="" class="article">
-                <img src={image3} alt="" />
+                <img src={image4} alt="" />
                 <div class="desc">
                     <span class="titre">Butterfly Sets: How to Dominate Your Next 100m Butterfly</span>
                     <span>“You gotta go out fast, and come back faster!” is one of those lines ...</span>
                 </div>            </a>
 
             <a href="" class="article">
-                <img src={image4} alt="" />
+                <img src={image5} alt="" />
                 <div class="desc">
-                    <span class="titre">This is How Fast Katie Ledecky Swims in Practice</span>
+                    <span class="titre">This is How Fast Katie Ledecky Swims.</span>
                     <span>You already know that Katie Ledecky is the top female swimmer ...</span>
                 </div>            </a>
         </div>
